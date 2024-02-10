d40f5b8d8c42c226f198fe06506565774adb5658
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-02-10 23:26:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,8 +66,9 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("• Update •", url="https://t.me/jn_bots"), InlineKeyboatdButton("help", callback_data="help")]]), reply_to_message_id=message.id)
+	if len(message.command) != 2:
+	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**",
+ elif  reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("• Update •", url="https://t.me/jn_bots"), InlineKeyboatdButton("help", callback_data="help")]]), reply_to_message_id=message.id)
 query.data == "help":
         buttons = [[
             InlineKeyboardButton(' ✯ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ✯', url='https://t.me/jn_bots')
