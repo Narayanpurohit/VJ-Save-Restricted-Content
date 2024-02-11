3735c07c20c25ff4d26cf4833fac8134d5d91eae
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-02-11 06:09:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ import json
 
 with open('config.json', 'r') as f: DATA = json.load(f)
 def getenv(var): return os.environ.get(var) or DATA.get(var, None)
-
-PICS = (environ.get('PICS', 'https://graph.org/file/a34b90e123b1e9195eb1e.jpg')).split()
 bot_token = getenv("TOKEN") 
 api_hash = getenv("HASH") 
 api_id = getenv("ID")
