d128fcb682d6957992376ec0f3be3a3fc590b97e
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-02-11 06:11:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def progress(current, total, message, type):
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**",
-reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("• Update •", url="https://t.me/jn_bots"), InlineKeyboatdButton("help", callback_data="help")]]), reply_to_message_id=message.id)
+reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("• Update •", url="https://t.me/jn_bots"), InlineKeyboardButton("help", callback_data="help")]]), reply_to_message_id=message.id)
 
 @bot.on_callback_query()
 def callback_query(client, CallbackQuery):
