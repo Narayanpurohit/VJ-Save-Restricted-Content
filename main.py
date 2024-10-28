d53a194bb78845dda0128e3b77ef7d3da9edb371
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-10-28 21:06:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import threading
 import json
 from os import environ
 
-bot_token = environ.get("TOKEN", "") 
-api_hash = environ.get("HASH", "") 
-api_id = int(environ.get("ID", ""))
+bot_token = environ.get("TOKEN", "6338089128:AAEEaqfshglYA-fGAzF5nLA0_YYLzdejCSU") 
+api_hash = environ.get("HASH", "0ef9fdd3e5f1ed49d4eb918a07b8e5d6") 
+api_id = int(environ.get("ID", "26847865"))
 bot = Client("mybot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 ss = environ.get("STRING", "")
